a13fbe8901520471816e77f0dd2fb48b2f152d69
Hassan <Hassan.akhtar44@yahoo.com>
2021-11-20 13:11:00 +0500

    login completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "./components/card";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [users, setUsers] = useState([]);
+	useEffect(() => {
+		const fetchData = async () => {
+			const res = await axios.get("https://reqres.in/api/users?page=2");
+			// console.log(res);
+			setUsers(res.data.data);
+		};
+		fetchData();
+	}, []);
+	return (
+		<div className='App'>
+			{users.map((user, i) => (
+				<Card key={user.id}>
+					<Card.Image src={user.avatar}></Card.Image>
+					<Card.body>
+						<Card.Title>
+							{user.first_name} {user.last_name}
+						</Card.Title>
+						<Card.Text>{user.email}</Card.Text>
+						<Card.Button>Details</Card.Button>
+					</Card.body>
+				</Card>
+			))}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/card/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container, Image, Body, Title, Text, Button } from "./style/card";
+export default function Card({ children, ...restProps }) {
+	return <Container {...restProps}>{children}</Container>;
+}
+
+Card.Image = function CardImage({ src, alt, ...restProps }) {
+	return <Image src={src} alt={alt} {...restProps}></Image>;
+};
+
+Card.body = function CardBody({ children, ...restProps }) {
+	return <Body {...restProps}>{children}</Body>;
+};
+Card.Title = function CardTitle({ children, ...restProps }) {
+	return <Title {...restProps}>{children}</Title>;
+};
+Card.Text = function CardText({ children, ...restProps }) {
+	return <Text {...restProps}>{children}</Text>;
+};
+Card.Button = function CardText({ children, ...restProps }) {
+	return <Button {...restProps}>{children}</Button>;
+};
